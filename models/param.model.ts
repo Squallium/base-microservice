5edf0b5f4af31e6a6de6b660c0f04b099e08b2b5
Borja Refoyo Ruiz <brefoyo@generagames.com>
2020-11-27 16:45:12 +0100

    product categories listo

---
+++ models/param.model.ts
@@ -0,0 +1,11 @@
+// Lazy Begin Imports
+import {Document} from "mongoose";
+// Lazy End Imports
+
+// Lazy Begin
+export interface IParam extends Document {
+    name: string;
+    description: string;
+}
+
+// Lazy End
