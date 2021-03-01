db225ceea7d0078ca5aac9532bd7f5388e44011f
Borja Refoyo Ruiz <borja.refoyo@scopely.com>
2021-03-01 22:11:20 +0100

    update de reviewers casi terminado voy a hacer un hotfix

--- utils/controller.util.ts
+++ utils/controller.util.ts
@@ -5,6 +5,7 @@ export class ControllerUtil {
 
     static manageUnknownError(error, errorClass: any, callback: (err: BaseError, response: BaseResponse) => void) {
         if (error && error.code && error.message) {
+            console.error(error.message)
             callback(new errorClass(500, error.message, error.code), null);
         } else {
             callback(error ? error : BaseError.unknown(error.message, errorClass), null);
